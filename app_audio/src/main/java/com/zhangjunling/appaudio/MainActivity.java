a0045c6da5b2fe238eb000a903ec7ea3e7b645e6
zhangjunlig <jizhang@renosdata.com>
2018-08-14 14:35:23 +0800

    通过android 上层的AudioRecord录制音频文件

--- app_audio/src/main/java/com/zhangjunling/appaudio/MainActivity.java
+++ app_audio/src/main/java/com/zhangjunling/appaudio/MainActivity.java
@@ -1,24 +1,30 @@
 package com.zhangjunling.appaudio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-
+    private Button mAudioRecordRecorderView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_audio);
+        mAudioRecordRecorderView = this.findViewById(R.id.bt_audiorecord_recorder);
 
+        mAudioRecordRecorderView.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-
-
+            case R.id.bt_audiorecord_recorder:
+                Intent audioRecordRecorderIntent = new Intent(MainActivity.this, AudioRecorderActivity.class);
+                startActivity(audioRecordRecorderIntent);
+                break;
         }
     }
 }
